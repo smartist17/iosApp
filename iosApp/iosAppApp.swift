fe9cdf1216d5af8a7ca03125dcda71798db36b2c
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-05-21 13:43:15 -0400

    Initial Commit

---
+++ iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by ashley on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
